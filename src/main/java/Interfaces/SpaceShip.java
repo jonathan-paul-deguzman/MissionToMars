a80c63f703b089e73911f635b1820e8a0ea7dc17
Jonathan Deguzman <deguzman.jonathan.paul@gmail.com>
2018-03-31 11:51:55 -0700

    Added JavaDoc comments.

--- src/main/java/Interfaces/SpaceShip.java
+++ src/main/java/Interfaces/SpaceShip.java
@@ -4,11 +4,35 @@ import main.java.Models.Item;
 
 public interface SpaceShip {
 
+    /**
+     * Determines the probability of successfully launching the Space Ship. Implementations of this method should
+     * calculate the probability of a successful launch.
+     *
+     * @return true or false based on the success of the launching
+     */
     boolean launch();
 
+    /**
+     * Determines the probability of successfully landing the Space Ship. Implementations of this method should
+     * calculate the probability of a successful landing.
+     *
+     * @return true or false based on the success of the landing
+     */
     boolean land();
 
+    /**
+     * Determines if the specified item can be added on to the Space Ship or not. Implementations of this method should
+     * determine the max amount of item weight the Space Ship can hold.
+     *
+     * @param item the item to potentially add as cargo
+     * @return true or false based on if adding the item can fit as cargo on the Space Ship
+     */
     boolean canCarry(Item item);
 
+    /**
+     * Adds the specified item to the Space Ship.
+     *
+     * @param item the item to add as cargo
+     */
     void carry(Item item);
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -12,46 +12,14 @@ import java.util.Scanner;
 
 public class Simulation {
 
-    public ArrayList<Rocket> loadU1Rockets(ArrayList<Item> itemsForLoading) {
-        ArrayList<Rocket> filledU1Rockets = new ArrayList<>();
-        Rocket rocket = new U1();
-        for (Item item : itemsForLoading) {
-            if (rocket.canCarry(item)) {
-                rocket.carry(item);
-            } else {
-                filledU1Rockets.add(rocket);
-                // Create new main.java.Models.U1 rocket and add our excess item to it
-                rocket = new U1();
-                rocket.carry(item);
-            }
-        }
-        // Add the last rocket (containing our final items) to the list
-        if (rocket.getCurrentRocketWeight() > rocket.getRocketWeight()) {
-            filledU1Rockets.add(rocket);
-        }
-        return filledU1Rockets;
-    }
-
-    public ArrayList<Rocket> loadU2Rockets(ArrayList<Item> itemsForLoading) {
-        ArrayList<Rocket> filledU2Rockets = new ArrayList<>();
-        Rocket rocket = new U2();
-        for (Item item : itemsForLoading) {
-            if (rocket.canCarry(item)) {
-                rocket.carry(item);
-            } else {
-                filledU2Rockets.add(rocket);
-                // Create a new main.java.Models.U2 rocket and add our excess item to it
-                rocket = new U2();
-                rocket.carry(item);
-            }
-        }
-        // Add the last rocket (containing our final items) to the list
-        if (rocket.getCurrentRocketWeight() > rocket.getRocketWeight()) {
-            filledU2Rockets.add(rocket);
-        }
-        return filledU2Rockets;
-    }
-
+    /**
+     * Loads the items from a text file. Each line contains the item name, followed by a '=', and its weight in Kgs.
+     *
+     * @param file the text file provided by the user
+     * @return items the ArrayList of {@link Item} objects
+     * @throws FileNotFoundException if the File is misspelled, not found, or invalid
+     * @throws NumberFormatException if the File contains lines that aren't of the format 'String=Integer'
+     */
     public ArrayList<Item> loadItems(File file) throws FileNotFoundException, NumberFormatException {
         ArrayList<Item> items = new ArrayList<>();
         Scanner input = new Scanner(file);
@@ -65,6 +33,66 @@ public class Simulation {
         return items;
     }
 
+    /**
+     * Creates U1 rockets by trying to fill up 1 rocket with as many items as possible before creating a new rocket
+     * object and filling that one until all items are loaded.
+     *
+     * @param itemsForLoading the ArrayList of items returned from the loadItems method
+     * @return filledU1Rockets the ArrayList of {@link U1} rocket objects that are fully loaded
+     */
+    public ArrayList<Rocket> loadU1Rockets(ArrayList<Item> itemsForLoading) {
+        ArrayList<Rocket> filledU1Rockets = new ArrayList<>();
+        Rocket rocket = new U1();
+        for (Item item : itemsForLoading) {
+            if (rocket.canCarry(item)) {
+                rocket.carry(item);
+            } else {
+                filledU1Rockets.add(rocket);
+                // Create new U1 rocket and add our excess item to it
+                rocket = new U1();
+                rocket.carry(item);
+            }
+        }
+        // Add the last rocket (containing our final items) to the list
+        if (rocket.getCurrentRocketWeight() > rocket.getRocketWeight()) {
+            filledU1Rockets.add(rocket);
+        }
+        return filledU1Rockets;
+    }
+
+    /**
+     * Creates U2 rockets by trying to fill up 1 rocket with as many items as possible before creating a new rocket
+     * object and filling that one until all items are loaded.
+     *
+     * @param itemsForLoading the ArrayList of items returned from the loadItems method
+     * @return filledU1Rockets the ArrayList of {@link U2} rocket objects that are fully loaded
+     */
+    public ArrayList<Rocket> loadU2Rockets(ArrayList<Item> itemsForLoading) {
+        ArrayList<Rocket> filledU2Rockets = new ArrayList<>();
+        Rocket rocket = new U2();
+        for (Item item : itemsForLoading) {
+            if (rocket.canCarry(item)) {
+                rocket.carry(item);
+            } else {
+                filledU2Rockets.add(rocket);
+                // Create a new U2 rocket and add our excess item to it
+                rocket = new U2();
+                rocket.carry(item);
+            }
+        }
+        // Add the last rocket (containing our final items) to the list
+        if (rocket.getCurrentRocketWeight() > rocket.getRocketWeight()) {
+            filledU2Rockets.add(rocket);
+        }
+        return filledU2Rockets;
+    }
+
+    /**
+     * Simulates launching and landing the rockets; every time a rocket explodes or crashes it must be sent again.
+     *
+     * @param rocketsForSimulation the ArrayList of {@link Rocket} objects returned from loadU1Rockets or loadU2Rockets
+     * @return totalBudget the total budget required to send each rocket safely to Mars
+     */
     public int runSimulation(ArrayList<Rocket> rocketsForSimulation) {
         int totalBudget = 0;
         for (Rocket rocket : rocketsForSimulation) {
